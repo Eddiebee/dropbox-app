a2b913fefe61857a60915b29e350672500201afa
eddiebee <edbeeotudor@gmail.com>
2021-12-27 15:03:51 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import { Dropbox } from "dropbox";
 import fetch from "node-fetch";
 
 const accessToken =
-  "CYZrovPe4vkAAAAAAAAAAZ6qjVYMvegtTPXx2XodmTmTiN5Teqq_41A06vlh8q2S";
+  "";
 
 const dbx = new Dropbox({
   accessToken,
